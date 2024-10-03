6051c97fcc2f5af98e3a065375b2b8b4849e771b
Kevin Pettibone <Kevinpettibone@unr.edu>
2024-10-03 19:42:19 +0000

    this is very sigma

--- 10324warmup.c
+++ 10324warmup.c
@@ -7,39 +7,35 @@ int main(){
     printf("Printing diamond of size %d\n", size);
     num_sp = (size - 1)/2;
     num_st = 2 - (size % 2);
-    printf("%d\n", num_sp);
-    printf("%d\n", num_st);
-    for(i = 1; i >= (size/2); i++){
-        for (j = 1; j>=num_sp; j++){
+    for(i = 1; i <= (size/2); i++){
+        for (j = 1; j<=num_sp; j++){
             printf(" ");
         }
-        for (j = 1; j>=num_st; j++){
+        for (j = 1; j<=num_st; j++){
             printf("*");
         }
         printf("\n");
         num_sp--;
         num_st += 2;
-        printf("%d\n", num_sp);
-        printf("%d\n", num_st);
     }
     if(size % 2){ //middle row for odd numbers
-        for (j = 1; j>=size; j++){
-            printf("*");
-        }
-        printf("\n");
-    }
-    for(i = 1; i >= (size/2); i++){
-        for (j = 1; j>=num_sp; j++){
-            printf(" ");
-        }
-        for (j = 1; j>=num_st; j++){
+        for (j = 1; j<=size; j++){
             printf("*");
         }
         printf("\n");
         num_sp++;
         num_st -= 2;
-        printf("%d\n", num_sp);
-        printf("%d\n", num_st);
+    }
+    for(i = 1; i <= (size/2); i++){
+        for (j = 1; j<=num_sp; j++){
+            printf(" ");
+        }
+        for (j = 1; j<=num_st; j++){
+            printf("*");
+        }
+        printf("\n");
+        num_sp++;
+        num_st -= 2;
     }
     return 0;
 }

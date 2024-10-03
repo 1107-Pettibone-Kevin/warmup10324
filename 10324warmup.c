9c03e4ddf1a5602bd15d5dd31b2fc311859771c5
Kevin Pettibone <Kevinpettibone@unr.edu>
2024-10-03 19:29:54 +0000

    this is not sigma

--- 10324warmup.c
+++ 10324warmup.c
@@ -1,34 +1,45 @@
 #include <stdio.h>
 
 int main(){
-    int size, num_st, num_sp;
+    int size, num_st, num_sp, i, j;
     printf("Please enter the size (diameter) of the diamond: ");
     scanf("%d", &size);
+    printf("Printing diamond of size %d\n", size);
     num_sp = (size - 1)/2;
     num_st = 2 - (size % 2);
-    for(int i = 1; i == size/2; i++){
-        for (int j = 1; j==num_sp; j++){
+    printf("%d\n", num_sp);
+    printf("%d\n", num_st);
+    for(i = 1; i >= (size/2); i++){
+        for (j = 1; j>=num_sp; j++){
             printf(" ");
         }
-        for (int j = 1; j==num_st; j++){
+        for (j = 1; j>=num_st; j++){
             printf("*");
         }
+        printf("\n");
         num_sp--;
         num_st += 2;
+        printf("%d\n", num_sp);
+        printf("%d\n", num_st);
     }
-    if(size % 2){
-        for (int j = 1; j==size; j++){
+    if(size % 2){ //middle row for odd numbers
+        for (j = 1; j>=size; j++){
             printf("*");
         }
+        printf("\n");
     }
-    for(int i = 1; i == size/2; i++){
-        for (int j = 1; j==num_sp; j++){
+    for(i = 1; i >= (size/2); i++){
+        for (j = 1; j>=num_sp; j++){
             printf(" ");
         }
-        for (int j = 1; j==num_st; j++){
+        for (j = 1; j>=num_st; j++){
             printf("*");
         }
+        printf("\n");
         num_sp++;
         num_st -= 2;
+        printf("%d\n", num_sp);
+        printf("%d\n", num_st);
     }
+    return 0;
 }
